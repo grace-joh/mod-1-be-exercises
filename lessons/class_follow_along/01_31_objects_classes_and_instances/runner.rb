aef90db8fc349f6bb8d27c89243af8c0a2863695
Grace Joh <graceehjoh@gmail.com>
2023-01-31 11:57:03 -0600

    Add updated unicorn code along files

---
+++ lessons/class_follow_along/01_31_objects_classes_and_instances/runner.rb
@@ -0,0 +1,7 @@
+# ~/turing/mod1/../01_31_objects_classes_and_instances/runner.rb
+require './unicorn'
+
+charlie = Unicorn.new('Charlie')
+unicorn_2 = Unicorn.new('Erin')
+
+require 'pry'; binding.pry
---
+++ lessons/class_follow_along/01_31_objects_classes_and_instances/unicorn.rb
@@ -0,0 +1,9 @@
+class Unicorn
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+  
