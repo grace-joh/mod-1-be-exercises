89b6d0ea5290b5f0ec00c164d84e2e548d744cd8
Grace Joh <graceehjoh@gmail.com>
2023-02-07 23:00:07 -0600

    test: add test for Potluck #menu

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Potluck do
     @roast_pork = Dish.new("Roast Pork", :entree)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
     @candy_salad = Dish.new("Candy Salad", :dessert)
+    @bean_dip = Dish.new("Bean Dip", :appetizer)
   end
   
   describe '#initialize' do
@@ -40,6 +41,22 @@ RSpec.describe Potluck do
       expect(@potluck.get_all_from_category(:appetizer)).to eq([@couscous_salad, @summer_pizza])
       expect(@potluck.get_all_from_category(:appetizer).first).to eq(@couscous_salad)
       expect(@potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
+    end
+  end
+
+  describe '#menu' do
+    it 'returns a hash with category key and value dishes array alphabetized' do
+      @potluck.add_dish(@couscous_salad)
+      @potluck.add_dish(@summer_pizza)
+      @potluck.add_dish(@roast_pork)
+      @potluck.add_dish(@cocktail_meatballs)
+      @potluck.add_dish(@candy_salad)
+      @potluck.add_dish(@bean_dip)
+      
+      expect(@potluck.menu).to be_a(Hash)
+      expect(@potluck.menu[:appetizer]).to eq(["Couscous Salad", "Summer Pizza", "Bean Dip"])
+      expect(@potluck.menu[:entree]).to eq(["Roast Pork", "Cocktail Meatballs"])
+      expect(@potluck.menu[:dessert]).to eq(["Candy Salad"])   
     end
   end
 end
