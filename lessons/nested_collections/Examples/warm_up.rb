0dc98c1c9d78f6c26eaf9837f35f3310f35caf3c
Grace Joh <graceehjoh@gmail.com>
2023-02-14 10:33:56 -0600

    add nested warmup

--- lessons/nested_collections/Examples/warm_up.rb
+++ lessons/nested_collections/Examples/warm_up.rb
@@ -1,12 +1,30 @@
 animals = {"dogs" => 3, "cats" => 5, "iguanas" => 2}
 
 # 1. return the value of dogs
+# p animals["dogs"]
+
 # 2. add 3 parakeets
+# animals["parakeets"] =3
+# p animals
+
 # 3. increase the amount of cats by 2
+# animals["cats"] += 2
+# p animals["cats"]
 
 pet_names = ["Fela", "Spot", "Patch", "Willy"]
 
 # 1. add “Claude” to the end of the array
+# pet_names << "Claude"
+# p pet_names
+
 # 2. access the first name in the list
+# p pet_names.first
+
 # 3. access the last name in the list
+# p pet_names.last
+
 # 4. remove “Fela” from the list
+# p pet_names.shift
+# p pet_names.delete("Fela")
+# p pet_names
+
