9f1d0d7919b011d0ff31da68020d2320c361c5b3
Grace Joh <graceehjoh@gmail.com>
2023-02-15 08:58:10 -0600

    feat: add Groomer charges and #add_charge

--- practice_independent_challenges/week3/pets_and_customers/practice/lib/groomer.rb
+++ practice_independent_challenges/week3/pets_and_customers/practice/lib/groomer.rb
@@ -1,9 +1,11 @@
 class Groomer
-  attr_reader :name, :customers
+  attr_reader :name, :customers, :services, :charges
 
   def initialize(name)
     @name = name
     @customers = []
+    @services = {}
+    @charges = []
   end
 
   def add_customer(customer)
@@ -20,5 +22,20 @@ class Groomer
       customer.pets.each { |pet| count += 1 if pet.type.to_s == pet_type }
     end
     count
+  end
+
+  def add_service(service_type, fee)
+    @services[service_type] = fee
+    @services
+  end
+
+  def add_charge(type, customer, pet)
+    charge_hash = {
+      type: type,
+      cost: @services[type],
+      customer: customer,
+      pet: pet
+    }
+    @charges << charge_hash
   end
 end
