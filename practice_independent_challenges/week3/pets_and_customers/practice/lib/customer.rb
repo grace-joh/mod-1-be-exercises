310cd990b3f6250b18e107f0e7a585614dec98d4
Grace Joh <graceehjoh@gmail.com>
2023-02-14 21:59:59 -0600

    feat: add Customer #charge

--- practice_independent_challenges/week3/pets_and_customers/practice/lib/customer.rb
+++ practice_independent_challenges/week3/pets_and_customers/practice/lib/customer.rb
@@ -1,13 +1,18 @@
 class Customer
-  attr_reader :name, :id, :pets
+  attr_reader :name, :id, :pets, :outstanding_balance
 
   def initialize(name, id)
     @name = name
     @id = id
     @pets = []
+    @outstanding_balance = 0
   end
 
   def adopt(pet)
     @pets << pet
+  end
+
+  def charge(fee)
+    @outstanding_balance += fee
   end
 end
