bedecbe494890022e41f0ca88282fdca4fce4763
Grace Joh <graceehjoh@gmail.com>
2023-02-14 21:44:40 -0600

    test: add tests for Pet #fed? and #feed

--- practice_independent_challenges/week3/pets_and_customers/practice/spec/pet_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/practice/spec/pet_spec.rb
@@ -1,25 +1,38 @@
 require 'spec_helper'
 
 RSpec.describe Pet do
+  before(:each) do
+    @samson = Pet.new({name: "Samson", type: :dog, age: 3})
+    @lily = Pet.new({name: "Lily", type: :cat, age: 1})
+  end
+
   describe '#initialize' do
     it 'can initialize' do
-      samson = Pet.new({name: "Samson", type: :dog, age: 3})
-
-      expect(samson).to be_a Pet
-      expect(samson.name).to eq("Samson")
-      expect(samson.type).to eq(:dog)
-      expect(samson.age).to eq(3)
-      expect(samson.fed).to eq(false)
+      expect(@samson).to be_a Pet
+      expect(@samson.name).to eq("Samson")
+      expect(@samson.type).to eq(:dog)
+      expect(@samson.age).to eq(3)
+      expect(@samson.fed).to eq(false)
     end
 
     it 'can initialize another pet' do
-      lily = Pet.new({name: "Lily", type: :cat, age: 1})
+      expect(@lily).to be_a Pet
+      expect(@lily.name).to eq("Lily")
+      expect(@lily.type).to eq(:cat)
+      expect(@lily.age).to eq(1)
+      expect(@lily.fed).to eq(false)
+    end
+  end
 
-      expect(lily).to be_a Pet
-      expect(lily.name).to eq("Lily")
-      expect(lily.type).to eq(:cat)
-      expect(lily.age).to eq(1)
-      expect(lily.fed).to eq(false)
+  describe '#fed? and #feed' do
+    it 'can return false if not fed' do
+      expect(@samson.fed?).to be false
+    end
+
+    it 'can return true after feeding' do
+      @samson.feed
+
+      expect(@samson.fed?).to be true
     end
   end
 end
