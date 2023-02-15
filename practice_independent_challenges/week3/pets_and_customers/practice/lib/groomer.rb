44377f0a83b823c3a2ee2faef65a257585b3386c
Grace Joh <graceehjoh@gmail.com>
2023-02-15 08:35:34 -0600

    feat: add Groomer #count_by_type

--- practice_independent_challenges/week3/pets_and_customers/practice/lib/groomer.rb
+++ practice_independent_challenges/week3/pets_and_customers/practice/lib/groomer.rb
@@ -9,4 +9,16 @@ class Groomer
   def add_customer(customer)
     @customers << customer
   end
+
+  def customers_with_unpaid_balance
+    @customers.select { |customer| customer.outstanding_balance.positive? }
+  end
+
+  def count_by_type(pet_type)
+    count = 0
+    @customers.each do |customer|
+      customer.pets.each { |pet| count += 1 if pet.type.to_s == pet_type }
+    end
+    count
+  end
 end
