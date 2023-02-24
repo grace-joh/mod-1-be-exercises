3c673be34a2345330aa4e80976fd8f347b67e536
Grace Joh <graceehjoh@gmail.com>
2023-02-24 08:28:18 -0600

    refactor: Add another contestant for testing

--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 
 RSpec.describe Game do
   before(:each) do
-    @alexander = Contestant.new({first_name: 'Alexander',
-                                last_name: 'Aigiades',
-                                age: 28,
-                                state_of_residence: 'CO',
-                                spending_money: 10})
+    @alexander = Contestant.new({ first_name: 'Alexander',
+                                  last_name: 'Aigiades',
+                                  age: 28,
+                                  state_of_residence: 'CO',
+                                  spending_money: 10 })
+    @leo = Contestant.new({ first_name: 'Leo',
+                            last_name: 'Joh',
+                            age: 18,
+                            state_of_residence: 'TX',
+                            spending_money: 8 })
   end
 
   describe '#initialize' do
@@ -17,6 +22,15 @@ RSpec.describe Game do
       expect(@alexander.age).to eq(28)
       expect(@alexander.state_of_residence).to eq('CO')
       expect(@alexander.spending_money).to eq(10)
+    end
+
+    it 'can create another contestant' do
+      expect(@leo).to be_a Contestant
+      expect(@leo.first_name).to eq('Leo')
+      expect(@leo.last_name).to eq('Joh')
+      expect(@leo.age).to eq(18)
+      expect(@leo.state_of_residence).to eq('TX')
+      expect(@leo.spending_money).to eq(8)
     end
   end
 
