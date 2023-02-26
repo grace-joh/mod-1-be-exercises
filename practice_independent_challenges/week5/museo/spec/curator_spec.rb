5647e827f23034e5506773da44b0d4e109350911
Grace Joh <graceehjoh@gmail.com>
2023-02-26 10:52:45 -0600

    test: add test for Curator #initialize

---
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+RSpec.describe Curator do
+  let(:curator) { Curator.new }
+  let(:photo1) do
+    Photograph.new({ id: '1',
+                     name: 'Rue Mouffetard, Paris (Boy with Bottles)',
+                     artist_id: '4',
+                     year: '1954' })
+  end
+  let(:photo2) do
+    Photograph.new({ id: '2',
+                     name: 'Moonrise, Hernandez',
+                     artist_id: '2',
+                     year: '1941' })
+  end
+  let(:artist1) do
+    Artist.new({ id: '1',
+                 name: 'Henri Cartier-Bresson',
+                 born: '1908',
+                 died: '2004',
+                 country: 'France' })
+  end
+  let(:artist2) do
+    Artist.new({ id: '2',
+                 name: 'Ansel Adams',
+                 born: '1902',
+                 died: '1984',
+                 country: 'United States' })
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(curator).to be_a Curator
+      expect(curator.photographs).to eq([])
+      expect(curator.artists).to eq([])
+    end
+  end
+end
