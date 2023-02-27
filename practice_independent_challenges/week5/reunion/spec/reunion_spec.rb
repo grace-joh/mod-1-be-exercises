82657e084256d14d2e5786fe9d84d063166b0b73
Grace Joh <graceehjoh@gmail.com>
2023-02-26 21:39:57 -0600

    feat: Add Reunion #add_activity

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 RSpec.describe Reunion do
   let(:reunion) { Reunion.new('2301 BE') }
-  let(:activity) { Activity.new('Brunch') }
+  let(:activity1) { Activity.new('Brunch') }
 
   describe '#initialize' do
     it 'exists' do
       expect(reunion).to be_a Reunion
       expect(reunion.name).to eq('2301 BE')
       expect(reunion.activities).to eq([])
+    end
+  end
+
+  describe '#add_activity' do
+    it 'adds an activity to the activities array' do
+      reunion.add_activity(activity1)
+      
+      expect(reunion.activities).to eq([activity1])
     end
   end
 end
